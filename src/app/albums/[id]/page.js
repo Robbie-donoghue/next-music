d769c4b1fc5397746ffa5bf653736a8cd932640e
Robbie Donoghue <Leon.Wh1te@proton.me>
2024-02-29 15:07:36 +0000

    for deployment

---
+++ src/app/albums/[id]/page.js
@@ -0,0 +1,15 @@
+export default Page({ params });
+{
+  //api stuff, once deployed from vercel
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.rows.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/albums/page.js
@@ -0,0 +1,6 @@
+export default function Page() {
+  return (
+    //album api stuff
+    <h1>Albums</h1>
+  );
+}
